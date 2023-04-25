d84b1153e41f1fe54d473631da6a11e8e0da1502
V642642 <83902292+V642642@users.noreply.github.com>
2023-04-25 11:57:29 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,22 @@
-const Banner =  dynamic(() => import("../components/Banner"), { ssr: true }); // from "../components/Banner";
+const Banner = dynamic(() => import("../components/Banner"), { ssr: true }); // from "../components/Banner";
 import KeyBenifit from "../components/KeyBenifit";
 import Marquee from "../components/Marquee";
 import Parallex from "../components/Parallex";
 import Social from "../components/Social";
 import ProductSlider from "../components/ProductSlider";
-const Clients =  dynamic(() => import("../components/Clients"), { ssr: false }); // from "../components/Clients";
+const Clients = dynamic(() => import("../components/Clients"), { ssr: false }); // from "../components/Clients";
 import Footer from "../components/Footer";
 import OurClient from "../components/OurClient";
 import Request from "../components/Employer/Request";
 import dynamic from "next/dynamic";
 import { createClient } from "../prismicio";
-import Blog from "../components/Blog"
+import Blog from "../components/Blog";
 import { useState } from "react";
+import axios from "axios";
 const WhyHp = dynamic(() => import("../components/WhyHp"), { ssr: false });
 
-export default function Home({ marquee, cards }) {
-  
- 
+export default function Home({ marquee, cards, blog }) {
 
-  
   return (
     <div className="w-full relative flex items-center justify-center flex-col overflow-x-hidden">
       {/* <Marquee  marquee={marquee?.data} /> */}
@@ -29,7 +27,7 @@ export default function Home({ marquee, cards }) {
       <Parallex />
       <ProductSlider />
       <OurClient />
-      <Blog />
+      <Blog blog={blog} />
       <Social />
       {/* <Request /> */}
       <Footer />
@@ -39,15 +37,22 @@ export default function Home({ marquee, cards }) {
 
 export async function getStaticProps({ previewData }) {
   const client = createClient({ previewData });
+  try {
+    const blogCards = await axios.get(
+      `https://www.hiringplug.com/blog_json.php`
+    );
+    const blog = blogCards.data
+    const marquee = await client.getSingle("marquee");
+    const cards = await client.getAllByType("clientcards");
 
-  const marquee = await client.getSingle("marquee");
-  const cards = await client.getAllByType("clientcards");
-  return {
-    props: {
-      marquee,
-      cards,
-    },
-  };
+    return {
+      props: {
+        marquee,
+        cards,
+        blog,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-
